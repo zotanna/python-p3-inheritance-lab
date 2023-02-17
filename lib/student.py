a26240a36cab05ac6b2ea6bb9a83107ea9e40733
Jerrica Bobadilla <jbobadilla27@gmail.com>
2023-02-17 13:01:39 -0800

    remove solution

--- lib/student.py
+++ lib/student.py
@@ -3,9 +3,6 @@
 from user import User
 
 class Student(User):
-    def __init__(self, first_name, last_name):
-        super().__init__(first_name, last_name)
-        self.knowledge = []
-
-    def learn(self, knowledge_string):
-        self.knowledge.append(knowledge_string)
+    
+    def learn(self):
+        pass
