b089dc38c06f92eddcfadc1c46d95ff99b99818f
denis <denniiskim@gmail.com>
2024-01-22 03:46:21 +0300

    completed assignment

--- lib/student.py
+++ lib/student.py
@@ -4,5 +4,9 @@ from user import User
 
 class Student(User):
     
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name, knowledge = []):
+        super().__init__(first_name, last_name)
+        self.knowledge = knowledge
+
+    def learn(self, new_information):
+        self.knowledge.append(new_information)
--- lib/user.py
+++ lib/user.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
 
 class User:
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
     pass
